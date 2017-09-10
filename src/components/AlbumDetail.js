f262db08377b3618aa67e8594bf1ef58041943e8
Frido1 <fjmartinez3@gmail.com>
2017-09-10 19:02:13 -0400

    new file

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import React, { Component } from "react";
+
+import { StyleSheet, View } from "react-native";
+
+const AlbumDetail = () => {
+	retun(
+		<View>
+			<Text>{props.album.title}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 	state = { albums: [] };
 
 	componentWillMount() {
+		//html get with a javacript promise(callback success, callback fail)
 		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-		.then(response => this.setState({ albums: response.data }));	
+		.then(response => this.setState({ albums: response.data }), 
+			response => console.warn(response));	
 	}
 
 	renderAlbums() {
 		return this.state.albums.map(album => 
-			<Text key={album.title}>{album.title}</Text>);
+			<AlbumDetail key={album.title} album={album} />
+			);
 	}
 
 	render() {
